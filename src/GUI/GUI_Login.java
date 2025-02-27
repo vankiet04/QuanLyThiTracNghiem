4ec1aab9f5c4967e50cf2cf4f2edf87faed5923b
Thuy An <diepthuyandta.@gmail.com>
2025-02-27 15:35:35 +0700

    them init login

--- src/GUI/GUI_Login.java
+++ src/GUI/GUI_Login.java
@@ -20,6 +20,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.JFrame;
+import javax.swing.*;
 import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
 public class GUI_Login extends JFrame{
@@ -30,10 +31,12 @@ public class GUI_Login extends JFrame{
 
 
     public GUI_Login() {
+        InitComponent();
         this.setSize(new Dimension(1000, 600));
         this.setTitle("HỆ THỐNG QUẢN LÝ THI TRẮC NGHIỆM");
         this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     }
     
     private void InitComponent(){
